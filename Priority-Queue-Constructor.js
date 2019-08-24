d4c66bedf32bc675fc20f689c8260695eb3e340b
Thomas Luo <tluo9713@gmail.com>
2019-08-24 11:30:50 -0400

    added a simple priority queue

---
+++ Priority-Queue-Constructor.js
@@ -0,0 +1,18 @@
+//Naive implementation of the priority queue. Naive because the solution is
+//slow, inserting and sorting at O(nlogn) time instead of O(logn)
+
+export default class PriorityQueue {
+  constructor() {
+    this.values = [];
+  }
+  enqueue(val, priority) {
+    this.values.push({ val, priority });
+    this.sort();
+  }
+  dequeue() {
+    return this.values.shift();
+  }
+  sort() {
+    this.values.sort((a, b) => a.priority - b.priority);
+  }
+}
