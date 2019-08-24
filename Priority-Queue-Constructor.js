585f0f9b128163a28fdcd6d8a947f47e1317b7aa
Thomas Luo <tluo9713@gmail.com>
2019-08-24 15:55:43 -0400

    weighted graph with dijkstras algo

--- Priority-Queue-Constructor.js
+++ Priority-Queue-Constructor.js
@@ -16,3 +16,64 @@ export default class PriorityQueue {
     this.values.sort((a, b) => a.priority - b.priority);
   }
 }
+
+class MaxBinaryHeap {
+  constructor() {
+    this.heap = [];
+  }
+  enqueue(val, priority) {
+    this.heap.push({ val, priority });
+    this.bubbleUp();
+  }
+  bubbleUp() {
+    let index = this.heap.length - 1;
+    let parentIdx = Math.floor(index / 2);
+    let heapArr = this.heap;
+    let parentEl = heapArr[parentIdx].val;
+    let indexEl = heapArr[index].val;
+
+    while (parentEl < indexEl) {
+      //Reassign parent and child element
+      heapArr[parentIdx] = indexEl;
+      heapArr[index] = parentEl;
+      //Reassign indices
+      index = parentIdx;
+      parentIdx = Math.floor(index / 2);
+      parentEl = heapArr[parentIdx].val;
+      indexEl = heapArr[index].val;
+    }
+  }
+  dequeue() {
+    let heapArr = this.heap;
+    let temp = heapArr[0];
+    heapArr[0] = heapArr[heapArr.length - 1];
+    heapArr[heapArr.length - 1] = temp;
+    let max = heapArr.pop();
+    this.bubbleDown();
+    return max;
+  }
+  bubbleDown() {
+    let index = 0;
+    let heapArr = this.heap;
+    let left = index * 2 + 1;
+    let right = index * 2 + 2;
+    let parentEl = heapArr[index];
+    let leftEl = heapArr[left];
+    let rightEl = heapArr[right];
+    let maxIdx = leftEl < rightEl ? right : left;
+    let maxEl = leftEl < rightEl ? rightEl : leftEl;
+    while (maxEl > parentEl) {
+      let temp = parentEl;
+      heapArr[index] = maxEl;
+      heapArr[maxIdx] = temp;
+      index = maxIdx;
+      left = index * 2 + 1;
+      right = index * 2 + 2;
+      parentEl = heapArr[index];
+      leftEl = heapArr[left];
+      rightEl = heapArr[right];
+      maxIdx = leftEl < rightEl ? right : left;
+      maxEl = leftEl < rightEl ? rightEl : leftEl;
+    }
+  }
+}
--- Weighted-Graph-Constructor.js
+++ Weighted-Graph-Constructor.js
@@ -1,4 +1,18 @@
-import PriorityQueue from './Priority-Queue-Constructor';
+class PriorityQueue {
+  constructor() {
+    this.values = [];
+  }
+  enqueue(val, priority) {
+    this.values.push({ val, priority });
+    this.sort();
+  }
+  dequeue() {
+    return this.values.shift();
+  }
+  sort() {
+    this.values.sort((a, b) => a.priority - b.priority);
+  }
+}
 
 class WeightedGraph {
   constructor() {
@@ -12,14 +26,80 @@ class WeightedGraph {
     this.adjacencyList[vertex1].push({ node: vertex2, weight });
     this.adjacencyList[vertex2].push({ node: vertex1, weight });
   }
+  DijkstraInitialize(start, distances, prev, pQ) {
+    for (let vertex in this.adjacencyList) {
+      if (this.adjacencyList.hasOwnProperty(vertex)) {
+        if (vertex === start) {
+          distances[vertex] = 0;
+          pQ.enqueue(vertex, 0);
+        } else {
+          distances[vertex] = Infinity;
+          pQ.enqueue(vertex, Infinity);
+        }
+        prev[vertex] = null;
+      }
+    }
+  }
+  DijkstraSearch(smallest, distances, prev, pQ) {
+    for (let neighbor in this.adjacencyList[smallest]) {
+      if (this.adjacencyList[smallest].hasOwnProperty(neighbor)) {
+        console.log('alist', this.adjacencyList[smallest]);
+        let nextNode = this.adjacencyList[smallest][neighbor];
+        console.log('neighbor', nextNode.node);
+
+        let candidate = distances[smallest] + nextNode.weight;
+        let nextNeighbor = nextNode.node;
+        if (candidate < distances[nextNeighbor]) {
+          distances[nextNeighbor] = candidate;
+          prev[nextNeighbor] = smallest;
+          pQ.enqueue(nextNeighbor, candidate);
+        }
+        console.log('pq', pQ.values);
+        console.log('distance', distances);
+      }
+    }
+  }
   DijkstraAlgo(start, end) {
     let distances = {};
-    this.adjacencyList.forEach(node => {
-      if (distances[node] === start) {
-        distances[node] = 0;
-      } else {
-        distances[node] = Infinity;
+    let prev = {};
+    let pQ = new PriorityQueue();
+    let smallest;
+    let path = [];
+    this.DijkstraInitialize(start, distances, prev, pQ);
+    while (pQ.values.length) {
+      smallest = pQ.dequeue().val;
+      if (smallest === end) {
+        while (prev[smallest]) {
+          path.push(smallest);
+          smallest = prev[smallest];
+        }
+        break;
       }
-    });
+      // if (smallest || distances[smallest] !== Infinity) {
+      this.DijkstraSearch(smallest, distances, prev, pQ);
+      // }
+    }
+    let newPath = path.concat(smallest).reverse();
+    console.log(newPath);
+    return newPath;
   }
 }
+
+let g = new WeightedGraph();
+g.addVertex('a');
+g.addVertex('b');
+g.addVertex('c');
+g.addVertex('d');
+g.addVertex('e');
+g.addVertex('f');
+
+g.addEdge('a', 'b', 4);
+g.addEdge('a', 'c', 2);
+g.addEdge('b', 'e', 3);
+g.addEdge('c', 'd', 2);
+g.addEdge('c', 'f', 4);
+g.addEdge('d', 'e', 3);
+g.addEdge('d', 'f', 1);
+g.addEdge('e', 'f', 1);
+
+g.DijkstraAlgo('a', 'e');
